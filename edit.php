edf1549544e6987b3a6f856321d63be767e9a8ae
Константин Обухов <obuchov.k@gmail.com>
2017-01-18 01:22:22 +0300

    add edit.php del editor.php add new.php

--- edit.php
+++ edit.php
@@ -2,9 +2,6 @@
 
 require __DIR__ . '/autoload.php';
 
-$news = \App\Models\Article::findAll ();
-$article = new \App\Models\Article();
-
 if (isset($_GET['id'])) {
     $article = \App\Models\Article::findById($_GET['id']);
 }
